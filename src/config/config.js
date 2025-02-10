3d7c8e1da4f9d3128b7683ee63ce86e2c3a732d0
Saekoboyy <Saekoboyy@gmail.com>
2025-02-10 00:05:08 -0500

    new file:   .gitignore 	new file:   src/commands/information/about.js 	new file:   src/commands/information/ping.js 	new file:   src/commands/staff/endSupport.js 	new file:   src/commands/staff/helpdesk.js 	new file:   src/config/config.js 	new file:   src/config/support-settings.js 	new file:   src/index.js 	new file:   src/package.json 	new file:   src/scripts/interactions.js 	new file:   src/scripts/logging.js 	new file:   src/scripts/snippets.js

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+const config = {
+
+    presence: "online", // options: online, idle, dnd, invisible
+    status: "Tech_Guy07's permission management is cringe.",
+
+
+    // Colors!!!
+    //accent: "Hex code here! Dont forget to remove the //!",
+    embed: "2c2d31",
+    green: "3BA55C",
+    red: "F04747",
+    orange: "FAA61A",
+}
+
+
+module.exports = config
